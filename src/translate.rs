ba7baaf8cccfa935cc27874a82717b70e9e42df0
Amir Heidari <amir.heidari.khoram@gmail.com>
2023-03-07 17:17:23 +0330

    fix: fix issue of translating multiline text

--- src/translate.rs
+++ src/translate.rs
@@ -1,29 +1,42 @@
 use crate::iso_639::LanguageCodes;
-use regex::Regex;
 use reqwest::get;
+use serde_json::Value;
 
-// FIXME: regex does not select all elements, when we have more than one sentence in the text
 pub async fn translate(
     source_lang: LanguageCodes,
     target_lang: LanguageCodes,
     text: String,
 ) -> Result<String, ()> {
-    use lazy_static::lazy_static;
-
-    lazy_static! {
-        static ref TRANSLATION_REGEX: Regex = Regex::new(r#"^\[\[\["(.+?)".*$"#).unwrap();
-    }
-
     let url = format!(
         "https://translate.googleapis.com/translate_a/single?client=gtx&sl={}&tl={}&dt=t&q={}",
         source_lang.as_ref(),
         target_lang.as_ref(),
         text
     );
-    let req_response = get(&url).await.unwrap().text().await.unwrap();
-    if let Some(captures) = TRANSLATION_REGEX.captures(&req_response) {
-        Ok(captures[1].to_string())
-    } else {
-        Err(())
+    let req_response_raw = match get(&url).await.unwrap().text().await {
+        Ok(res) => res,
+        Err(_) => return Err(()),
+    };
+    let req_response: Value = match serde_json::from_str(req_response_raw.as_str()) {
+        Ok(res) => res,
+        Err(_) => return Err(()),
+    };
+    let translations = match req_response[0].as_array() {
+        Some(res) => res,
+        None => return Err(()),
+    };
+
+    let mut translated_text = String::new();
+    for translation in translations {
+        if !translated_text.is_empty() {
+            translated_text.push_str(" ");
+        }
+        let translated_sentence = match translation[0].as_str() {
+            Some(res) => res,
+            None => return Err(()),
+        };
+        translated_text.push_str(translated_sentence);
     }
+
+    Ok(translated_text)
 }
